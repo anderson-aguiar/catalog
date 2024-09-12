7d984c4f1330e79a84bf11ee1a0296b45fe5c331
Anderson Martins <anderson.aguiar@outlook.com.br>
2024-09-12 15:14:50 -0300

    Created  User and Role

--- backend/src/main/java/com/anderson/catalog/entities/Category.java
+++ backend/src/main/java/com/anderson/catalog/entities/Category.java
@@ -1,32 +1,39 @@
 package com.anderson.catalog.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="tb_category")
+@Table(name = "tb_category")
 public class Category {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant createdAt;
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
-	
 
-	public Category() {}
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
+
+	public Category() {
+	}
+
 	public Category(Long id, String name) {
 		this.id = id;
 		this.name = name;
@@ -47,20 +54,29 @@ public class Category {
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Instant getCreatedAt() {
 		return createdAt;
 	}
+
 	public Instant getUpdatedAt() {
 		return updatedAt;
 	}
+
 	@PrePersist
 	public void prePersist() {
 		createdAt = Instant.now();
 	}
+
 	@PreUpdate
 	public void preUpdate() {
 		updatedAt = Instant.now();
 	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -77,6 +93,5 @@ public class Category {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
