21f368395156a43b9ef849be36c926764e4758be
Anderson Martins <anderson.aguiar@outlook.com.br>
2024-09-12 15:26:21 -0300

    Database seeding

--- backend/src/main/java/com/anderson/catalog/entities/User.java
+++ backend/src/main/java/com/anderson/catalog/entities/User.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +27,8 @@ public class User implements Serializable {
 	private String email;
 	private String password;
 	
+	@ManyToMany
+	@JoinTable(name="tb_user_role", joinColumns = @JoinColumn(name="user_id"), inverseJoinColumns = @JoinColumn(name="role_id"))
 	private Set<Role> roles = new HashSet<>();
 	
 	public User() {}
